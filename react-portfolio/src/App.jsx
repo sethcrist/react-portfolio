38eadadf1a7c84c00d11fe27582a295250bf2ac3
Seth Crist <sethcrist50@gmail.com>
2024-03-18 18:46:44 -0500

    added some img and started on experience

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import styles from './App.module.css'
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import About from "./components/About/About.jsx";
+import Experience from "./components/Experience/Experience.jsx"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   )
 }
---
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import skills from "../../Data/skills.json"
+import {getImageUrl} from "../../utils.js";
+
+export const Experience = () => {
+    return (
+        <section id="experience">
+            <h2>Experience</h2>
+            <div>
+                <div>{
+                    skills.map((skills, id) => {
+                        return <div key={id}>
+                            <div><img src={getImageUrl(skills.imageSrc)} alt={skills.title}/></div>
+                        </div>
+                    })
+                }</div>
+                <ul></ul>
+            </div>
+        </section>
+    );
+}
+
+export default Experience;
