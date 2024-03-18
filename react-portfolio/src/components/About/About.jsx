04a915725c5c3eb52e3615b92f73ef22f380c9ad
Seth Crist <sethcrist50@gmail.com>
2024-03-18 14:10:37 -0500

    added an about section

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import {getImageUrl} from "../../utils.js";
+import styles from "./About.module.css"
 
 export const About = () => {
     return (
-        <section>
-            <h2>About</h2>
-            <div>
-                <img src={getImageUrl("about/about.png")} alt="sitting with a laptop"/>
+        <section className={styles.container} id="about">
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
+                <img src={getImageUrl("about/about.png")} alt="sitting with a laptop" className={styles.aboutImage}/>
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Frontend Developer</h3>
+                            <p>I'm a frontend developer with experience in building optimized and reactive sites</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/serverIcon.png")} alt="server icon"/>
+                        <div className={styles.aboutItemText}>
+                            <h3>Backend Developer</h3>
+                            <p>I have experience in developing fast and optimised websites as well as using APIs</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/uiIcon.png")} alt="ui icon"/>
+                        <div className={styles.aboutItemText}>
+                            <h3>UI Developer</h3>
+                            <p>I have designed and styled multiple websites using bootstrap and CSS</p>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </section>
     );
