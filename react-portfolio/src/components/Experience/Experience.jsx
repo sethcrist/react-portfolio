17e4dfb76d3e012a6b78a799a99023e336653d57
Seth Crist <sethcrist50@gmail.com>
2024-03-19 10:47:37 -0500

    added an experience section

--- react-portfolio/src/components/Experience/Experience.jsx
+++ react-portfolio/src/components/Experience/Experience.jsx
@@ -1,23 +1,50 @@
 import React from 'react';
 import skills from "../../Data/skills.json"
+import history from "../../Data/history.json"
+import styles from "./Experience.module.css"
 import {getImageUrl} from "../../utils.js";
+
 
 export const Experience = () => {
     return (
-        <section id="experience">
-            <h2>Experience</h2>
-            <div>
-                <div>{
-                    skills.map((skills, id) => {
-                        return <div key={id}>
-                            <div><img src={getImageUrl(skills.imageSrc)} alt={skills.title}/></div>
-                        </div>
-                    })
-                }</div>
-                <ul></ul>
+        <section className={styles.container} id="experience">
+            <h2 className={styles.title}>Experience</h2>
+            <div className={styles.content}>
+                <div className={styles.skills}>
+                    {skills.map((skills, id) => {
+                        return (
+                            <div key={id} className={styles.skill}>
+                                <div className={styles.skillImageContainer}>
+                                    <img src={getImageUrl(skills.imageSrc)} alt={skills.title}/>
+                                </div>
+                                <p>{skills.title}</p>
+                            </div>
+                        );
+                    })}
+                </div>
+                <ul className={styles.history}>
+                    {history.map((historyItem, id) => {
+                        return (
+                            <li key={id} className={styles.historyItem}>
+                                <img
+                                    src={getImageUrl(historyItem.imageSrc)}
+                                    alt={`${historyItem.organisation} Logo`}
+                                />
+                                <div className={styles.historyItemDetails}>
+                                    <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                                    <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                                    <ul>
+                                        {historyItem.experiences.map((experience, id) => {
+                                            return <li key={id}>{experience}</li>
+                                        })}
+                                    </ul>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
             </div>
         </section>
     );
 }
-
 export default Experience;
--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -19,13 +19,13 @@ export const Navbar = () => {
                     alt="menu-button"
                     onClick={() => setMenuOpen(!menuOpen)}
                 />
-                <ul className={`${styles.menuItems} $${menuOpen && styles.menuOpen}`}
+                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
                 onClick={() => setMenuOpen(false)}
                 >
-                    <li><a href="#about"></a>About</li>
-                    <li><a href="#experience"></a>Experience</li>
-                    <li><a href="#projects"></a>Projects</li>
-                    <li><a href="#contact"></a>Contact</li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
         </nav>
