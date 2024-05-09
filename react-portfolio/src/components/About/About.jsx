c4e6e01a8ec62e23d8f912cc35e0312083653ccc
Seth Crist <sethcrist50@gmail.com>
2024-05-09 14:07:24 -0500

    added more projects and images

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -12,14 +12,14 @@ export const About = () => {
                         <img src={getImageUrl("about/cursorIcon.png")} alt="cursor icon" />
                         <div className={styles.aboutItemText}>
                             <h3>Frontend Developer</h3>
-                            <p>I Utilize HTML, CSS, and JavaScript to turn design concepts into interactive, visually appealing web pages that users interact with directly. These web pages are reactive with different screen sizes.</p>
+                            <p>I Utilize HTML, CSS, React and JavaScript to turn design concepts into interactive, visually appealing web pages that users can interact with. These web pages are reactive with different screen sizes.</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/serverIcon.png")} alt="server icon"/>
                         <div className={styles.aboutItemText}>
                             <h3>Backend Developer</h3>
-                            <p>I have experience in leveraging APIs for seamless data interchange, managing sophisticated databases with PostgreSQL, and adapting various development frameworks to engineer robust web solutions.</p>
+                            <p>I have experience in Rails, leveraging APIs for seamless data interchange, managing databases with PostgreSQL, and adapting various development frameworks to engineer robust web solutions.</p>
                         </div>
                     </li>
                     <li className={styles.aboutItem}>
--- react-portfolio/src/components/Projects/ProjectCards.jsx
+++ react-portfolio/src/components/Projects/ProjectCards.jsx
@@ -19,7 +19,7 @@ export const ProjectCards = ({ project }) => {
                 })}
             </ul>
             <div className={styles.links}>
-                {/*<a href={demo} className={styles.link}>Demo</a>*/}
+                <a href={demo} className={styles.link} target="_blank">Demo</a>
                 <a href={source} className={styles.link} target="_blank">Source</a>
             </div>
         </div>
